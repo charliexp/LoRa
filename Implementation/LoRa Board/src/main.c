6ab7c92e5b0a5b5a07d80191e83ff7a8bc3af02b
Florin Popescu <florinsgpopescu@gmail.com>
2018-06-23 22:28:56 +0300

    Temp code to output meter data

--- Implementation/LoRa Board/src/main.c
+++ Implementation/LoRa Board/src/main.c
@@ -14,14 +14,22 @@ int main(void)
 	HW_Init();
 	
 	Comp_Init();
-	/*LoRa_Init();
+	LoRa_Init();
 	Meter_Init();
 	PC_Init();
 	
 	PC_SignalPressence();
-	*/
+	
+	Frame_t frame;
+	frame.endDevice = 0xAA;
+	frame.nrOfMessages = 1;
+	frame.messages[0].command = COMMAND_ACQUISITION;
+	frame.messages[0].argLength = 0;
+	
   while(1)
   {
+		LoRa_ProcessRequest(frame);
+		HAL_Delay(5000);
   }
 }
 
