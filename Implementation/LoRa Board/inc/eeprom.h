153a1d3e6cbe6bba27445e323ad607dff9d73e8a
Florin Popescu <florinsgpopescu@gmail.com>
2018-06-23 03:23:57 +0300

    EEPROM driver

---
+++ Implementation/LoRa Board/inc/eeprom.h
@@ -0,0 +1,29 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __EEPROM_H__
+#define __EEPROM_H__
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+   
+/* Includes ------------------------------------------------------------------*/
+#include "hw.h"
+
+/* Exported defines ----------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* External variables --------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+void EEPROM_EraseWord(uint32_t address);
+uint8_t EEPROM_ReadByte(uint32_t address);
+uint16_t EEPROM_ReadHalfWord(uint32_t address);
+uint32_t EEPROM_ReadWord(uint32_t address);
+void EEPROM_WriteByte(uint32_t address, uint8_t value);
+void EEPROM_WriteHalfWord(uint32_t address, uint16_t value);
+void EEPROM_WriteWord(uint32_t address, uint32_t value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __EEPROM_H__*/
---
+++ Implementation/LoRa Board/src/eeprom.c
@@ -0,0 +1,62 @@
+/* Includes ------------------------------------------------------------------*/
+#include "eeprom.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private constants ---------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Public variables ----------------------------------------------------------*/
+/* Functions Definition ------------------------------------------------------*/
+void EEPROM_EraseWord(uint32_t address)
+{
+		HAL_FLASHEx_DATAEEPROM_Unlock();
+		HAL_FLASHEx_DATAEEPROM_Erase(address);
+		HAL_FLASHEx_DATAEEPROM_Lock();
+}
+
+uint8_t EEPROM_ReadByte(uint32_t address)
+{
+	return *((uint8_t *) address);
+}
+
+uint16_t EEPROM_ReadHalfWord(uint32_t address)
+{
+	return *((uint16_t *) address);
+}
+
+uint32_t EEPROM_ReadWord(uint32_t address)
+{
+	return *((uint32_t *) address);
+}
+
+void EEPROM_WriteByte(uint32_t address, uint8_t value)
+{
+	if (*((uint8_t *) address) != value)
+	{
+		HAL_FLASHEx_DATAEEPROM_Unlock();
+		HAL_FLASHEx_DATAEEPROM_Program(FLASH_TYPEPROGRAMDATA_BYTE, address, value);
+		HAL_FLASHEx_DATAEEPROM_Lock();
+	}
+}
+
+void EEPROM_WriteHalfWord(uint32_t address, uint16_t value)
+{
+	if (*((uint16_t *) address) != value)
+	{
+		HAL_FLASHEx_DATAEEPROM_Unlock();
+		HAL_FLASHEx_DATAEEPROM_Program(FLASH_TYPEPROGRAMDATA_HALFWORD, address, value);
+		HAL_FLASHEx_DATAEEPROM_Lock();
+	}
+}
+
+void EEPROM_WriteWord(uint32_t address, uint32_t value)
+{
+	if (*((uint32_t *) address) != value)
+	{
+		HAL_FLASHEx_DATAEEPROM_Unlock();
+		HAL_FLASHEx_DATAEEPROM_Program(FLASH_TYPEPROGRAMDATA_WORD, address, value);
+		HAL_FLASHEx_DATAEEPROM_Lock();
+	}
+}
